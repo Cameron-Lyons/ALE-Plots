224fe8f8ff533c28fd532bcd4f4d71ffad955c1f
Cameron Lyons <clyons@outmatch.com>
2020-08-26 17:14:42 -0500

    seperated categorical and numerical ale

--- _ALE.py
+++ _ALE.py
@@ -3,8 +3,13 @@
 import numpy as np
 import pandas as pd
 
+from sklearn.base import is_regressor
 from sklearn.utils import _safe_indexing
 from sklearn.utils.extmath import cartesian
+from sklearn.manifold import MDS
+from scipy.stats.mstats import mquantiles
+
+from statsmodels.distributions.empirical_distribution import ECDF  # Get rid of extra dependency
 
 def _quantiles_from_X(X, n_quantiles):
     """Generate a grid of points based on the quantiles of X.
@@ -28,25 +33,27 @@ def _quantiles_from_X(X, n_quantiles):
         array ``values[j]`` is either ``n_quantiles``, or the number of
         unique values in ``X[:, j]``, whichever is smaller.
     """
-     if n_quantiles <= 1:
+    if n_quantiles <= 1:
         raise ValueError("'n_quantiles' must be strictly greater than 1.")
-     values = []
-     for feature in range(X.shape[1]):
-         uniques = np.unique(_safe_indexing(X, feature, axis=1))
-        if uniques.shape[0] < n_quantiles:
-            # feature has low resolution use unique vals
-            axis = uniques
-        else:
-            # create axis based on percentiles and grid resolution
-            emp_percentiles = mquantiles(
-                _safe_indexing(X, feature, axis=1), prob=np.linspace(0, 1, n_quantiles)
-            )
+
+    values = []
+    for feature in range(X.shape[1]):
+        uniques = np.unique(_safe_indexing(X, feature, axis=1))
+    if uniques.shape[0] < n_quantiles:
+        # feature has low resolution use unique vals
+        emp_percentiles = uniques
+    else:
+        # create axis based on percentiles and grid resolution
+        emp_percentiles = mquantiles(
+            _safe_indexing(X, feature, axis=1), prob=np.linspace(0, 1, n_quantiles)
+        )
+
+    values.append(emp_percentiles)
 
     return cartesian(values), values
 
-def _calculate_accumulated_local_effects(est, grid, features, X, response_method):
-    predictions = []
-    averaged_predictions = []
+def _ale_for_numeric(est, grid, features, X, response_method):
+    """computes first order accumulated local efffects for a numeric feature"""
 
     # define the prediction_method (predict, predict_proba, decision_function).
     if is_regressor(est):
@@ -83,7 +90,29 @@ def _calculate_accumulated_local_effects(est, grid, features, X, response_method
     ).mean()
     b1 = np.array(a1.value_counts(sort=False))
     centers = (effects[:-1] + effects[1:]) / 2
-    predictions = []
+    ale = []
     for i, value in enumerate(effects.to_numpy()):
         predictions.append(value * b1[i])
-    return averaged_predictions, predictions
+    return ale
+
+def _ale_for_categorical(est, grid, features, X, response_method):
+    """computes first order accumulated local efffects for a categorical feature"""
+    x_count = X[features].value_counts().sort_index().values
+    x_freq = x_count / sum(x_count) # probability vector for levels of X[:J]
+    K = len(X[features].unique()) # reset K to the number of levels of X[:J]
+    D_cum = np.zeros((K, K)) # will be the distance matrix between pairs of levels of X[:J]
+    D = np.zeros((K, K))
+    x_ecdf = {}
+    i = 0
+    for _, k in X.groupby(features)["CRIM"]:
+        x_ecdf[i] = ECDF(k)
+        i += 1
+    for i in range(0, K-1):
+        for k in range(i, K):
+            D[i][k] = max(abs(x_ecdf[i](grid) - x_ecdf[k](grid)))
+            D[k][i] = D[i][k]
+    embedding = MDS(n_components=1)
+    D1D = embedding.fit_transform(D)
+    ind_ord = sorted(range(len(D1D)), key=lambda k: D1D[k])
+    ord_ind = sorted(range(len(ind_ord)), key=lambda k: ind_ord[k])
+    levs_orig = X[features].unique()
