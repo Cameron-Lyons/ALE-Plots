8d75e1ddb0122559eabdab0035ba597be4746f88
Cameron Lyons <clyons@outmatch.com>
2020-08-27 19:46:47 -0500

    calculated distance matrix after loop

--- _ALE.py
+++ _ALE.py
@@ -6,16 +6,13 @@ import pandas as pd
 from sklearn.base import is_regressor
 from sklearn.utils import _safe_indexing
 from sklearn.utils.extmath import cartesian
-from sklearn.manifold import MDS
 from scipy.stats.mstats import mquantiles
 
-from statsmodels.distributions.empirical_distribution import ECDF  # Get rid of extra dependency
-
-def _quantiles_from_X(X, n_quantiles):
-    """Generate a grid of points based on the quantiles of X.
+def _quantiles_from_x(x, n_quantiles):
+    """Generate a grid of points based on the quantiles of x.
     Parameters
     ----------
-    X : ndarray, shape (n_samples, n_target_features)
+    x : ndarray, shape (n_samples, n_target_features)
         The data
 
     n_quantiles : int
@@ -26,33 +23,33 @@ def _quantiles_from_X(X, n_quantiles):
     -------
     grid : ndarray, shape (n_points, n_target_features)
         A value for each feature at each point in the grid. ``n_points`` is
-        always ``<= n_quantiles ** X.shape[1]``.
+        always ``<= n_quantiles ** x.shape[1]``.
 
     values : list of 1d ndarrays
         The values with which the grid has been created. The size of each
         array ``values[j]`` is either ``n_quantiles``, or the number of
-        unique values in ``X[:, j]``, whichever is smaller.
+        unique values in ``x[:, j]``, whichever is smaller.
     """
     if n_quantiles <= 1:
         raise ValueError("'n_quantiles' must be strictly greater than 1.")
 
     values = []
-    for feature in range(X.shape[1]):
-        uniques = np.unique(_safe_indexing(X, feature, axis=1))
+    for feature in range(x.shape[1]):
+        uniques = np.unique(_safe_indexing(x, feature, axis=1))
     if uniques.shape[0] < n_quantiles:
         # feature has low resolution use unique vals
         emp_percentiles = uniques
     else:
         # create axis based on percentiles and grid resolution
         emp_percentiles = mquantiles(
-            _safe_indexing(X, feature, axis=1), prob=np.linspace(0, 1, n_quantiles)
+            _safe_indexing(x, feature, axis=1), prob=np.linspace(0, 1, n_quantiles)
         )
 
     values.append(emp_percentiles)
 
     return cartesian(values), values
 
-def _ale_for_numeric(est, grid, features, X, response_method):
+def _ale_for_numeric(est, grid, features, x, response_method):
     """computes first order accumulated local efffects for a numeric feature"""
 
     # define the prediction_method (predict, predict_proba, decision_function).
@@ -73,46 +70,60 @@ def _ale_for_numeric(est, grid, features, X, response_method):
                     'The estimator has no predict_proba and no '
                     'decision_function method.'
                 )
-            elif response_method == 'predict_proba':
+            if response_method == 'predict_proba':
                 raise ValueError('The estimator has no predict_proba method.')
-            else:
-                raise ValueError(
-                    'The estimator has no decision_function method.')
-    X_eval = X.copy()
-    a1 = pd.cut(X_eval.iloc[:, 0], bins=grid[0], labels=False).fillna(0.0).astype(int)
-    X_eval_2 = X.copy()
-    X_eval_2.iloc[:, 0] = grid[0][a1 + 1]
-    y_hat = prediction_method(X_eval)
-    y_hat_2 = prediction_method(X_eval_2)
+            raise ValueError('The estimator has no decision_function method.')
+
+    x_eval = x.copy()
+    quantiles = pd.cut(x_eval.iloc[:, 0], bins=grid[0], labels=False).fillna(0.0).astype(int)
+    x_eval_2 = x.copy()
+    x_eval_2.iloc[:, 0] = grid[0][quantiles + 1]
+    y_hat = prediction_method(x_eval)
+    y_hat_2 = prediction_method(x_eval_2)
     delta = y_hat_2 - y_hat
-    effects  = pd.DataFrame({"a1": a1, "delta": delta}).groupby(
+    effects  = pd.DataFrame({"a1": quantiles, "delta": delta}).groupby(
         "a1"
     ).mean()
-    b1 = np.array(a1.value_counts(sort=False))
+    b1 = np.array(quantiles.value_counts(sort=False))
     centers = (effects[:-1] + effects[1:]) / 2
     ale = []
-    for i, value in enumerate(effects.to_numpy()):
-        predictions.append(value * b1[i])
+    for i, value in enumerate(centers.to_numpy()):
+        ale.append(value * b1[i])
     return ale
 
-def _ale_for_categorical(est, grid, features, X, response_method):
+def _ale_for_categorical(est, grid, features, x, response_method):
     """computes first order accumulated local efffects for a categorical feature"""
-    x_count = X[features].value_counts().sort_index().values
-    x_freq = x_count / sum(x_count) # probability vector for levels of X[:J]
-    K = len(X[features].unique()) # reset K to the number of levels of X[:J]
-    D_cum = np.zeros((K, K)) # will be the distance matrix between pairs of levels of X[:J]
-    D = np.zeros((K, K))
-    x_ecdf = {}
-    i = 0
-    for _, k in X.groupby(features)["CRIM"]:
-        x_ecdf[i] = ECDF(k)
-        i += 1
-    for i in range(0, K-1):
-        for k in range(i, K):
-            D[i][k] = max(abs(x_ecdf[i](grid) - x_ecdf[k](grid)))
-            D[k][i] = D[i][k]
-    embedding = MDS(n_components=1)
-    D1D = embedding.fit_transform(D)
-    ind_ord = sorted(range(len(D1D)), key=lambda k: D1D[k])
-    ord_ind = sorted(range(len(ind_ord)), key=lambda k: ind_ord[k])
-    levs_orig = X[features].unique()
+    if is_regressor(est):
+        prediction_method = est.predict
+    else:
+        predict_proba = getattr(est, 'predict_proba', None)
+        decision_function = getattr(est, 'decision_function', None)
+        if response_method == 'auto':
+            # try predict_proba, then decision_function if it doesn't exist
+            prediction_method = predict_proba or decision_function
+        else:
+            prediction_method = (predict_proba if response_method ==
+                                 'predict_proba' else decision_function)
+        if prediction_method is None:
+            if response_method == 'auto':
+                raise ValueError(
+                    'The estimator has no predict_proba and no '
+                    'decision_function method.'
+                )
+            if response_method == 'predict_proba':
+                raise ValueError('The estimator has no predict_proba method.')
+            raise ValueError('The estimator has no decision_function method.')
+
+    categories = x[features].value_counts().sort_index().values
+    n_categories = len(x[features].unique())
+    effects = np.zeros((n_categories, n_categories))
+    for i in range(n_categories):
+        x_eval = x[x_min.iloc[:, 0] == categories[i]]
+        x_min = x_eval.copy()
+        x_plus  = x_eval.copy()
+        x_min.iloc[:, 0] = grid[i - 1]
+        x_plus.iloc[:, 0] = grid[i + 1]
+        effects[i] += (prediction_method(x_plus) - prediction_method(x_min)).sum() / x_eval.shape[0]
+    distance_matrix = effects.cumsum()
+    ale = distance_matrix.mean()
+    return ale
