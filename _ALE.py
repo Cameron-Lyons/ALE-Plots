ca7d6c87f66efe59b82ecc0740700d5e00a67b38
Cameron Lyons <cameron.lyons2@gmail.com>
2020-08-30 23:11:57 -0500

    fixed bug in yhat not changing categories

--- _ALE.py
+++ _ALE.py
@@ -72,14 +72,11 @@ def _ale_for_numeric(est, grid, x, feature, response_method='auto'):
 
     x_eval = x.copy()
     x_feat = _safe_indexing(x, feature, axis=1)
-    print(x_feat)
     quantiles = pd.cut(x_feat, bins=grid, labels=False).fillna(0.0).astype(int)
     x_eval_2 = x.copy()
     x_feat_incremented = _safe_indexing(x_eval_2, feature, axis=1)
     x_feat_incremented = grid[quantiles + 1]
-    print(x_feat_incremented)
     x_eval_2[feature] = x_feat_incremented
-    print(x_eval_2)
     y_hat = prediction_method(x_eval)
     y_hat_2 = prediction_method(x_eval_2)
     delta = y_hat_2 - y_hat
@@ -93,7 +90,7 @@ def _ale_for_numeric(est, grid, x, feature, response_method='auto'):
         ale.append(value * quantile_counts[i])
     return ale
 
-def _ale_for_categorical(est, grid, x, response_method='auto'):
+def _ale_for_categorical(est, grid, x, feature,response_method='auto'):
     """computes first order accumulated local efffects for a categorical feature"""
     if is_regressor(est):
         prediction_method = est.predict
@@ -115,16 +112,16 @@ def _ale_for_categorical(est, grid, x, response_method='auto'):
             if response_method == 'predict_proba':
                 raise ValueError('The estimator has no predict_proba method.')
             raise ValueError('The estimator has no decision_function method.')
-
-    categories = x.value_counts().sort_index().values
-    n_categories = len(x.unique())
+    x_feat = _safe_indexing(x, feature, axis=1)
+    categories = x_feat.value_counts().sort_index().values
+    n_categories = len(x_feat.unique())
     effects = np.zeros((n_categories, n_categories))
     for i in range(n_categories):
-        x_eval = x[x_min.iloc[:, 0] == categories[i]]
-        x_min = x_eval.copy()
+        x_eval = x[x_feat == categories[i]]
+        x_min = x_feat.copy()
         x_plus  = x_eval.copy()
-        x_min.iloc[:, 0] = grid[i - 1]
-        x_plus.iloc[:, 0] = grid[i + 1]
+        x_min = grid[i - 1]
+        x_plus = grid[i + 1]
         effects[i] += (prediction_method(x_plus) - prediction_method(x_min)).sum() / x_eval.shape[0]
     distance_matrix = effects.cumsum()
     ale = distance_matrix.mean()
@@ -139,7 +136,7 @@ def accumulated_local_effects(est, x, feature, n_quantiles):
     quantiles = _quantiles_from_x(_safe_indexing(x, features_indices, axis=1), n_quantiles)
     x_feat = _safe_indexing(x, feature, axis=1)
     if x_feat.dtype.name == "category" or x_feat.dtype == "object":
-        ale = _ale_for_categorical(est, quantiles, x_feat)
+        ale = _ale_for_categorical(est, quantiles, x, feature)
     else:
         ale = _ale_for_numeric(est, quantiles, x, feature)
     return ale
